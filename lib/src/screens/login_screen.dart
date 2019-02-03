3dcb71a451196ba85f2eecd07e1da38abb81c1b3
vivek singh <viveksingh@viveks-iMac.local>
2019-02-03 23:21:27 +0530

    First initial commit

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.all(40.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            // passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+
+  }
+
+  Widget submitButton() {
+
+  }
+
+}
